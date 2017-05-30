a25f1c75dd18c82eedf80871098ac5af8c848ef5
asklong <499671517@qq.com>
2017-05-30 12:26:28 -0500

    Update 重新安排数据奇数位大于偶数位.java

--- Array/重新安排数据奇数位大于偶数位.java
+++ Array/重新安排数据奇数位大于偶数位.java
@@ -1,2 +1,6 @@
 //quick select + reorder O(N)
-
+//这题O(n)找中位数吧，然后大于中位数的放奇数位，小于的放偶数位
+//2 3 4 6 7 9 10, if we have odd numbers, we place the median 6 in the last place, and rearrange the other arrays.
+//if the array is 2 3 4 6 7 9, we have even numbers, we rearrange the number directly.
+//we check the element, if the odd place element is smaller, the even place is larger than median.
+//we find the 2 elements for swap.
